b2ef9d00857bc4775d76d188fc00b457cb8312ed
li <thebigbigwordl@qq.com>
2020-04-01 15:41:55 +0800

    fix cross chess match

--- #Chess/chess.rb
+++ #Chess/chess.rb
@@ -1,6 +1,7 @@
-Shoes.app(:width => 300, :height => 400, :resizable => false) do
+Shoes.app(:width => 300, :height => 400, :resizable => false, :title => "#Chess Game") do
   @map = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   @player = 1
+  Shoes.APPS[0].set_window_icon_path("ui-pic/icon.png")
   def draw_window
     clear() do
       stack(:width => 300, :height => 300, :top => 0, :left => 0) do
@@ -41,7 +42,7 @@ Shoes.app(:width => 300, :height => 400, :resizable => false) do
       for j in 0..2
         @line[i] = @line[i] + @map[i * 3 + j]
         @row[j] = @row[j] + @map[i * 3 + j]
-        @cross[0] = @cross[1] + @map[i * 3 + j] if i == j
+        @cross[0] = @cross[0] + @map[i * 3 + j] if i == j
         @cross[1] = @cross[1] + @map[i * 3 + j] if i + j == 2
       end
     end
